fb09a46ba8388e0ddca757d4524232f4920f2e65
ivanwarke <iveon.cc@gmail.com>
2018-10-16 00:21:55 +0800

    update

--- src/modules/login/LoginConnector.tsx
+++ src/modules/login/LoginConnector.tsx
@@ -1,14 +1,10 @@
 import * as React from "react";
-import { LoginController } from "@abb/controller";
 
-import { LoginView } from "./ui/LoginView";
 
 export class LoginConnector extends React.PureComponent {
   render() {
     return (
-      <LoginController>
-        {({ submit }) => <LoginView submit={submit} />}
-      </LoginController>
+     <div>1</div>
     );
   }
 }
